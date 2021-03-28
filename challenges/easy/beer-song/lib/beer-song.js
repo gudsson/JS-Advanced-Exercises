3e8d9b0343ade9d8368109df8514aad04c6e9e35
Jay Gudsson <gudsson@gmail.com>
2021-03-28 12:32:41 -0700

    beer-song complete

--- challenges/easy/beer-song/lib/beer-song.js
+++ challenges/easy/beer-song/lib/beer-song.js
@@ -1,2 +1,48 @@
-Beer Song
-Write a program that can generate the lyrics of the 99 Bottles of Beer song. See the test suite for the entire song.
+///////////////
+// Beer Song //
+///////////////
+
+// Write a program that can generate the lyrics of the 99 Bottles
+// of Beer song. See the test suite for the entire song.
+
+class BeerSong {
+  static verse(bottles) {
+    if (bottles === 0) return BeerSong.LAST_VERSE;
+    return BeerSong._getTypicalVerse(bottles);
+  }
+
+  static verses(firstVerse, lastVerse) {
+    let res = [];
+
+    for (let bottles = firstVerse; bottles >= lastVerse; bottles--) {
+      res.push(BeerSong._getTypicalVerse(bottles));
+    }
+
+    if (lastVerse === 0) {
+      res.pop();
+      res.push(BeerSong.LAST_VERSE)
+    };
+
+    return res.join(`\n`);
+  }
+
+  static lyrics() {
+    return BeerSong.verses(99, 0);
+  }
+
+  static _getTypicalVerse(bottles) {
+    return `${bottles} ${bottles === 1 ? 'bottle' : 'bottles'} of beer on ` +
+    `the wall, ${bottles} ${bottles === 1 ? 'bottle' : 'bottles'} of ` +
+    `beer.\nTake ${bottles > 1 ? 'one' : 'it'} down and pass it around,` +
+    ` ${(bottles - 1) === 0 ? 'no more' : bottles - 1} ${bottles - 1 === 1 ? 'bottle' : 'bottles'} ` +
+    `of beer on the wall.\n`;
+  }
+
+  static LAST_VERSE = `No more bottles of beer on the wall, no more ` +
+  `bottles of beer.\nGo to the store and buy some more, 99 bottles ` +
+  `of beer on the wall.\n`;
+}
+
+BeerSong.verses(99, 98)
+
+module.exports = BeerSong;
