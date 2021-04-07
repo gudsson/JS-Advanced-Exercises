043db0f11b54ceb2ed33bcecbc1f57cf0c81004a
Jay Gudsson <gudsson@gmail.com>
2021-04-06 23:54:35 -0700

    finish review2

--- lesson_2/review2/side-effects/pp2.js
+++ lesson_2/review2/side-effects/pp2.js
@@ -1,23 +1,28 @@
 // Which of the following functions are pure functions?
 
-Function 1
+// Function 1 => not pure
 function sum(a, b) {
   console.log(a + b);
   return a + b;
 }
-Function 2
+
+// Function 2 => PURE
 function sum(a, b) {
   a + b;
 }
-Function 3
+
+// Function 3 => PURE
 function sum(a, b) {
   return a + b;
 }
-Function 4
+
+// Function 4 => not pure
 function sum(a, b) {
   return a + b + Math.random();
 }
-Function 5
+// Function 5 => PURE
 function sum(a, b) {
   return 3.1415;
 }
+
+// Functions 2, 3, and 5 are pure functions.
